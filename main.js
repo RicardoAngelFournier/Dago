c256509bc29951a0c0b31499cc14c23572719a0d
RicardoAngelFournier <75813716+RicardoAngelFournier@users.noreply.github.com>
2022-02-15 08:43:48 -0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,8 @@
+
+var alumnos=['Maria','Jesus','Jose'];
+
+for(var j=0; j<alumnos.length;j++)
+    {
+        document.write('alumnos: '+(j+1)+ 'soy: ' + alumnos[j]+'<br>');
+    }
+    document.write('<br><hr>');
